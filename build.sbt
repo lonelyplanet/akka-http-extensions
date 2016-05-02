e8466297f0d5d8340fb9aaba439fa31655e85238
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-05-02 15:44:55 +0200

    basic code working

--- build.sbt
+++ build.sbt
@@ -1,6 +1,48 @@
+import com.typesafe.sbt.SbtScalariform
+import com.typesafe.sbt.SbtScalariform.ScalariformKeys
+
+import scalariform.formatter.preferences._
+
 name := "akka-http-extensions"
 
 version := "0.1"
 
 scalaVersion := "2.11.8"
-    
+
+javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
+
+scalacOptions ++= Seq(
+  "-encoding",
+  "utf8",
+  "-feature",
+  "-language:postfixOps",
+  "-language:implicitConversions",
+  "-unchecked",
+  "-deprecation"
+)
+
+
+resolvers ++= Seq("OSS" at "http://oss.sonatype.org/content/repositories/releases/")
+
+
+libraryDependencies ++= {
+  val akkaVersion = "2.3.15" //downgraded because of akkaStream requirements/compatibility
+  val akkaStreamVersion = "2.0.4"
+
+  Seq(
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion % "provided",
+    "com.typesafe.akka" %% "akka-stream-experimental" % akkaStreamVersion % "provided",
+    "com.typesafe.akka" %% "akka-http-core-experimental" % akkaStreamVersion % "provided",
+    "com.typesafe.akka" %% "akka-http-experimental" % akkaStreamVersion % "provided",
+    "com.typesafe.akka" %% "akka-http-spray-json-experimental" % akkaStreamVersion % "provided"
+  )
+}
+
+
+SbtScalariform.scalariformSettings
+
+ScalariformKeys.preferences := ScalariformKeys.preferences.value
+  .setPreference(AlignSingleLineCaseStatements, true)
+  .setPreference(DoubleIndentClassDeclaration, true)
+  .setPreference(SpacesAroundMultiImports, false)
+  .setPreference(CompactControlReadability, false)
---
+++ src/main/scala/com/wlangiewicz/akka/http/extensions/PaginationDirectives.scala
@@ -0,0 +1,53 @@
+package com.wlangiewicz.akka.http.extensions
+
+import akka.http.scaladsl.server.{Directive1, Rejection}
+import com.wlangiewicz.utils.Config._
+import akka.http.scaladsl.server.Directives._
+import scala.collection.immutable.Seq
+
+trait PaginationDirectives {
+
+  private val IndexParam = Config.get[String]("akka.http.extensions.pagination.index-param-name") || "page"
+  private val SizeParam = Config.get[String]("akka.http.extensions.pagination.size-param-name") || "size"
+  private val SortParam = Config.get[String]("akka.http.extensions.pagination.sort-param-name") || "sort"
+
+  private val AscParam = Config.get[String]("akka.http.extensions.pagination.asc-param-name") || "asc"
+  private val DescParam = Config.get[String]("akka.http.extensions.pagination.desc-param-name") || "desc"
+
+  private val SortingSeparator = Config.get[String]("akka.http.extensions.pagination.sorting-separator") || ";"
+  private val OrderSeparator = Config.get[Char]("akka.http.extensions.pagination.order-separator") || ','
+
+  def pagination: Directive1[Option[PageRequest]] =
+    parameterMap.flatMap { params =>
+      (params.get(IndexParam).map(_.toInt), params.get(SizeParam).map(_.toInt)) match {
+        case (Some(index), Some(size)) => provide(Some(deserializePage(index, size, params.get(SortParam))))
+        case (Some(index), None) => reject(MalformedPaginationRejection("Missing page size parameter", None))
+        case (None, Some(size)) => reject(MalformedPaginationRejection("Missing page index parameter", None))
+        case (_, _) => provide(None)
+      }
+    }
+
+  private def deserializePage(index: Int, size: Int, sorting: Option[String]) = {
+
+    val sortingParam = sorting.map(_.split(SortingSeparator).map(_.span(_ != OrderSeparator)).collect {
+      case (field, sort) if sort == ',' + AscParam => (field, Order.Asc)
+      case (field, sort) if sort == ',' + DescParam => (field, Order.Desc)
+    }.toMap)
+
+    PageRequest(index, size, sortingParam.getOrElse(Map.empty))
+  }
+
+  case class MalformedPaginationRejection(errorMsg: String, cause: Option[Throwable] = None) extends Rejection
+
+}
+
+sealed trait Order
+
+object Order {
+  case object Asc extends Order
+  case object Desc extends Order
+}
+
+case class PageRequest(index: Int, size: Int, sort: Map[String, Order])
+
+case class PageResponse[T](elements: Seq[T], totalElements: Int)
