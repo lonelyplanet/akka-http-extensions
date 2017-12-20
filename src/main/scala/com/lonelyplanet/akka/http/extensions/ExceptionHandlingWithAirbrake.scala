5aa2eb65b376da0ffbdc51b605afe0cd519afc25
Todor Kazakov <todd.kazakov@gmail.com>
2017-12-20 15:22:09 +0200

    Add urls to airbrake notifications

--- src/main/scala/com/lonelyplanet/akka/http/extensions/ExceptionHandlingWithAirbrake.scala
+++ src/main/scala/com/lonelyplanet/akka/http/extensions/ExceptionHandlingWithAirbrake.scala
@@ -1,17 +1,25 @@
 package com.lonelyplanet.akka.http.extensions
 
 import akka.http.scaladsl.server.ExceptionHandler
+import akka.http.scaladsl.server.directives.BasicDirectives._
 import com.lonelyplanet.akka.http.extensions.exceptions.ResourceNotFound
 import com.lonelyplanet.util.AirbrakeService
 
 trait ExceptionHandlingWithAirbrake extends ExceptionHandling {
   val airbrakeService: AirbrakeService
+  val airbrakeComponent: String = ExceptionHandlingWithAirbrake.DefaultAirbrakeComponent
 
   override implicit def exceptionHandler: ExceptionHandler = ExceptionHandler {
     case e: ResourceNotFound =>
       super.exceptionHandler.apply(e)
     case e =>
-      airbrakeService.notify(e)
-      super.exceptionHandler.apply(e)
+      extractUri { uri =>
+        airbrakeService.notify(e, uri.toString(), airbrakeComponent)
+        super.exceptionHandler.apply(e)
+      }
   }
+}
+
+object ExceptionHandlingWithAirbrake {
+  val DefaultAirbrakeComponent = "api"
 }
